0ec687327143935eeca023f77777877b8916cbbd
Johan Aberg <abergster@gmail.com>
2025-04-07 12:52:13 +1200

    window flags

--- tools/ld-analyse/blacksnranalysisdialog.cpp
+++ tools/ld-analyse/blacksnranalysisdialog.cpp
@@ -32,7 +32,8 @@ BlackSnrAnalysisDialog::BlackSnrAnalysisDialog(QWidget *parent) :
     ui(new Ui::BlackSnrAnalysisDialog)
 {
     ui->setupUi(this);
-    setWindowFlags(Qt::Window);
+    //setWindowFlags(Qt::Window);
+    setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowSystemMenuHint);
     setSizeGripEnabled(true);
 
     // Set up the chart view
