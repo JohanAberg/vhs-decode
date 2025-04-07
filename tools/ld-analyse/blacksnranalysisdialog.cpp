06c59d37d60bfb8e10a1882a33f0c211166a6e6a
Johan Aberg <abergster@gmail.com>
2025-04-07 12:15:01 +1200

    disable windowflags

--- tools/ld-analyse/blacksnranalysisdialog.cpp
+++ tools/ld-analyse/blacksnranalysisdialog.cpp
@@ -32,7 +32,7 @@ BlackSnrAnalysisDialog::BlackSnrAnalysisDialog(QWidget *parent) :
     ui(new Ui::BlackSnrAnalysisDialog)
 {
     ui->setupUi(this);
-    setWindowFlags(Qt::Window);
+    //setWindowFlags(Qt::Window);
 
     // Set up the chart view
     plot = new QwtPlot();
